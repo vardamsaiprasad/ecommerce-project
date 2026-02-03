c2bf38f3de3bb9a735dfec540f733f0000716589
Sai Prasad <psai30385@gmail.com>
2026-02-03 10:16:51 +0530

    Implement Task 2: Header and Navigation Bar, Hero Section

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,9 @@
 console.log("E-Commerce Website Loaded");
+
+// Hamburger menu toggle
+const hamburger = document.getElementById('hamburger');
+const nav = document.querySelector('.navbar nav');
+
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
